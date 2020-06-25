96b26789263099c757860dd26aa95730e061896e
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-06-25 18:13:44 +0100

    first commit

---
+++ who-buying-lucnh.js
@@ -0,0 +1,18 @@
+
+let names=["Angela", "Ben", "Jenny", "Cloe"];
+
+function whosPaying(names){
+    
+    return `${names[Math.floor(Math.random() * names.length)]} is going to buy lunch today!`;
+} 
+
+
+/*
+let names=["Angela", "Ben", "Jenny", "Cloe"];
+
+function whosPaying(names) {
+    return names[Math.floor(Math.random()*names.length)];
+}
+whosPaying(names);
+
+*/
